11d10d8d464f31bc2f90d34df355d3c8aabc5abd
stenoriom-ux <stenoriom@ucenfotec.ac.cr>
2026-06-08 18:41:30 -0600

    paso-1: implemento metodo fabrica
    paso-1: implemento metodo fabrica

--- src/main/kotlin/patterns/creational/FactoryMethod.kt
+++ src/main/kotlin/patterns/creational/FactoryMethod.kt
@@ -1,28 +1,37 @@
 package patterns.creational
 
-interface ProductoPendienteFactoryMethod {
+// Product
+interface Producto {
     fun descripcion(): String
 }
 
+// Concrete Product
+class Bicicleta : Producto {
+    override fun descripcion(): String = "bicicleta de montaña"
+}
+
+// Creator
 abstract class TallerPendiente {
+
     fun prepararPedido(): String {
-        val producto = crearProductoPendiente()
+        val producto = crearProducto()
         return "Pedido preparado para ${producto.descripcion()}"
     }
 
-    protected open fun crearProductoPendiente(): ProductoPendienteFactoryMethod {
-        // TODO: reemplaza este método por un verdadero método fábrica abstracto.
-        return object : ProductoPendienteFactoryMethod {
-            override fun descripcion(): String = "producto temporal"
-        }
+    // Factory Method
+    protected abstract fun crearProducto(): Producto
+}
+
+// Concrete Creator
+class TallerBicicletas : TallerPendiente() {
+    override fun crearProducto(): Producto {
+        return Bicicleta()
     }
 }
 
 class FactoryMethodDemo {
     fun ejecutar(): String {
-        // TODO: crea un creador concreto y úsalo desde aquí.
-        return TallerLocal().prepararPedido()
+        val taller: TallerPendiente = TallerBicicletas()
+        return taller.prepararPedido()
     }
 }
-
-class TallerLocal : TallerPendiente()
