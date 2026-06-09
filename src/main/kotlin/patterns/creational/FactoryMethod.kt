92ea1124977f41f2c6ed352e40fcb9f4bbf8697e
stenoriom-ux <stenoriom@ucenfotec.ac.cr>
2026-06-08 18:57:37 -0600

    "paso-1: implemento metodo fabrica"
    "paso-1: implemento metodo fabrica"

--- src/main/kotlin/patterns/creational/FactoryMethod.kt
+++ src/main/kotlin/patterns/creational/FactoryMethod.kt
@@ -1,5 +1,35 @@
-package patterns.creational
-
+// PRODUCT
 interface Product {
     fun description(): String
+}
+
+// CONCRETE PRODUCT
+class ProductoDigital : Product {
+    override fun description(): String = "producto digital descargable"
+}
+
+// CREATOR
+abstract class Creator {
+
+    fun createOrder(): String {
+        val product = createProduct()
+        return "Pedido preparado para ${product.description()}"
+    }
+
+    protected abstract fun createProduct(): Product
+}
+
+// CONCRETE CREATOR
+class DigitalStore : Creator() {
+    override fun createProduct(): Product {
+        return ProductoDigital()
+    }
+}
+
+// CLIENT
+class FactoryMethodDemo {
+    fun ejecutar(): String {
+        val creator: Creator = DigitalStore()
+        return creator.createOrder()
+    }
 }
